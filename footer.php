4efc6ca366fb590c39f97045bc9ce74ea7d13cf9
Tyghe Vallard <tyghe@minilore.localdomain>
2011-09-05 23:22:01 +0700

    Better footer

--- footer.php
+++ footer.php
@@ -7,7 +7,14 @@
 
 			</div>
 			<div id="footer" class="contentBackground">
-				Footer
+				<p>
+					<?php bloginfo('name'); ?> is proudly powered by
+					<a href="http://wordpress.org/">WordPress</a>&nbsp;
+					|&nbsp;&nbsp;<a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
+					and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+					<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+					<br />Blog theme custom made by Tyghe
+				</p>
 			</div>
 		</div>
 	<?php wp_footer(); ?>
